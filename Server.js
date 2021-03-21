cdefcfae2497c9d07fac2fe538923c12feeab377
JoakimDenlov <Joakimdenlov1@gmail.com>
2021-03-21 19:30:02 +0100

    test 150

--- Server.js
+++ Server.js
@@ -10,7 +10,6 @@ import CommentRoutes from  './src/routes/comment.routes.js';
 import TimelineRoutes from './src/routes/Timeline.routes.js';
 import path from 'path'
 
-
 dotenv.config();
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,15 +18,6 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan('common'));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'))
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')) // relative path
-    })
-  }
-
-
 CommentRoutes.routes(app);
 TimelineRoutes.routes(app);
 app.use(middleware.notFound);
@@ -35,3 +25,11 @@ app.use(middleware.errorHandler);
 
 Configuration.connectToDatabase();
 Configuration.connectToPort(app);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'))
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+  }

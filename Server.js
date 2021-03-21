47902d5621b49c979e16facbf25f63eba15a7518
JoakimDenlov <Joakimdenlov1@gmail.com>
2021-03-21 19:44:59 +0100

    another test

--- Server.js
+++ Server.js
@@ -19,10 +19,10 @@ app.use(cors());
 app.use(morgan('common'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('./frontend/build'))
+  app.use(express.static(__dirname + '/frontend/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   })
 }
 

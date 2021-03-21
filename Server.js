7287013888b855dd9e8ae56bb5d02d6571cdb719
JoakimDenlov <Joakimdenlov1@gmail.com>
2021-03-21 18:04:48 +0100

    test 6

--- Server.js
+++ Server.js
@@ -8,6 +8,7 @@ import middleware from './src/middleware/MiddleWare.js';
 import Configuration from './config/Configuration.js';
 import CommentRoutes from  './src/routes/comment.routes.js';
 import TimelineRoutes from './src/routes/Timeline.routes.js';
+import path from 'path'
 
 
 dotenv.config();
@@ -26,6 +27,10 @@ app.use(middleware.errorHandler);
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+      });
 
 }
 
